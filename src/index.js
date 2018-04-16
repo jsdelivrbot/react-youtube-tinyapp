9a8ce9a961da8538b2849e405215d1a9ab689471
Stefanos Kariotidis <stef.kariotidis@gmail.com>
2018-04-16 12:12:23 +0300

    throttle search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
+import _ from 'lodash'
 
 import SearchBar from './components/search-bar'
 import VideoList from './components/video-list'
@@ -14,15 +15,21 @@ class App extends Component {
 
     this.state = { videos: [] }
 
-    YTSearch({ key: API_KEY, term: 'blockchain and smart contracts' }, (videos) => {
+    this.videoSearch('blockchain and smart contracts')
+  }
+
+  videoSearch (term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
       this.setState({ videos })
     })
   }
 
   render () {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
